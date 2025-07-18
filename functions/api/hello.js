5399760f2f1d2b7b4513b8e9c9f19bd2c2d3c3e7
chihung1024 <104315542+chihung1024@users.noreply.github.com>
2025-07-18 14:28:11 +0800

    chore: remove hello test function

--- functions/api/hello.js
+++
@@ -1,7 +0,0 @@
-// Cloudflare Pages Functions 範例：GET /api/hello
-export const onRequestGet = () => {
-  return new Response(
-    JSON.stringify({ ok: true, msg: "Hello from Cloudflare Functions!" }),
-    { headers: { "content-type": "application/json" } }
-  );
-};
